f983c39f53014f0023934cfd3b5519fa56538b5b
willhoy61 <willhoy61@gmail.com>
2017-11-15 01:28:28 -0600

    tue js update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,86 @@
 // generate random number for 4 crystals
-let numToGet;
-var blueGem;
-var greenGem;
-var purpleGem;
-var redGem;
+var numToGet = 0;
+var blueGem = 0;
+var greenGem = 0;
+var purpleGem = 0
+var redGem = 0;
+var win = 0;
+var lose = 0;
+var currScore =0;
 
-
+function reset()
+{
+	
+	$("scoreCount, #blue, #greeen, #purple, #red").empty();	
+}
 
 $(document).ready(function()
 {
 	$("#scoreCount").on("click", function()
 	{
-		let numToGet = Math.floor((Math.random() * 100) + 1);
+		 numToGet = Math.floor((Math.random() * 100) + 1);
 		console.log(numToGet);
 	 $("#scoreCount").html(numToGet);
+
 	});
 	
 	
 $("#blue").on("click", function()
 	{
 	blueGem = Math.floor((Math.random() *10) + 1);
+	
+	if (blueGem <= numToGet)
+	{
+		blueGem += blueGem;
+	}
+	$("#blue").val(blueGem);
 	console.log(blueGem);
-	$("#blue").html(blueGem);
+	reset();
 	});
-$("#green").on("click", function(){
+$("#green").on("click", function()
+{
 	greenGem = Math.floor((Math.random() *10) + 1);
+	if(greenGem <= numToGet)
+	{
+		greenGem += greenGem;
+	}
+	$("#green").val(greenGem);
 	console.log(greenGem);
-	$("#green").html(greenGem);
+	return greenGem;
 });
 $("#purple").on("click", function(){
 	purpleGem = Math.floor((Math.random() *10) + 1);
+	if(purpleGem <= numToGet)
+	{
+		purpleGem += purpleGem;
+	}
+	$("#purple").val(purpleGem);
 	console.log(purpleGem);
-	$("#purple").html(purpleGem);
+	return purpleGem;
 });
 $("#red").on("click", function(){
 	redGem = Math.floor((Math.random() *10) + 1);
+	if(redGem <= numToGet)
+	{
+		redGem =+ redGem;
+	}
+	$("#green").val(redGem);
 	console.log(redGem);
-	$("#green").html(redGem);
+	return redGem;
 });
+if(blueGem && greenGem && purpleGem && redGem === numToGet)
+	{
+		$("#winLossButton").html("you Win");
+		win++;
+		$("#wins").html(win++);
+	}
+		if(blueGem && greenGem && purpleGem && redGem > numToGet)
+		{
+			$("winLossButton").html("You lose");
+			lose++;
+			$("#losses").html(lose++);
+		}
+reset();
  	
 
 
